cc7b801e82a52150b98ffa8c12e9f904e1f596b3
ReenieSmooch <smolchenko@gmail.com>
2023-07-16 12:52:57 +0300

    App's logic and base structure

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div className="header">
+      <div>Players</div>
+      <div>Game options</div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/hooks/useMemoryGameLogic.js
@@ -0,0 +1,84 @@
+import { useState, useEffect, useCallback, useMemo } from "react";
+
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
+const useMemoryGameLogic = () => {
+  const [cards, setCards] = useState([]);
+  const [flippedCards, setFlippedCards] = useState([]);
+  const [matchedCards, setMatchedCards] = useState([]);
+
+  const generateCards = useCallback(() => {
+    const symbols = [
+      "🐶",
+      "🐱",
+      "🐭",
+      "🐹",
+      "🐰",
+      "🐻",
+      "🐼",
+      "🐨",
+      "🦁",
+      "🐯",
+    ];
+    const cardPairs = symbols.concat(symbols);
+    return shuffleArray(cardPairs);
+  }, []);
+
+  const handleCardClick = useCallback(
+    (index) => {
+      if (
+        flippedCards.length < 2 &&
+        !flippedCards.includes(index) &&
+        !matchedCards.includes(index)
+      ) {
+        setFlippedCards((prevFlippedCards) => [...prevFlippedCards, index]);
+      }
+    },
+    [flippedCards, matchedCards]
+  );
+
+  const checkMatch = useCallback(() => {
+    if (flippedCards.length === 2) {
+      const [firstCard, secondCard] = flippedCards;
+      const newMatchedCards = [...matchedCards];
+
+      if (cards[firstCard] === cards[secondCard]) {
+        newMatchedCards.push(firstCard, secondCard);
+      }
+
+      setTimeout(() => {
+        setMatchedCards(newMatchedCards);
+        setFlippedCards([]);
+      }, 1000);
+    }
+  }, [flippedCards, matchedCards, cards]);
+
+  useEffect(() => {
+    checkMatch();
+  }, [checkMatch]);
+
+  const restartGame = useCallback(() => {
+    setCards(generateCards());
+    setFlippedCards([]);
+    setMatchedCards([]);
+  }, [generateCards]);
+
+  useEffect(() => {
+    setCards(generateCards());
+  }, [generateCards]);
+
+  const memoizedValues = useMemo(
+    () => ({ cards, flippedCards, matchedCards, handleCardClick, restartGame }),
+    [cards, flippedCards, matchedCards, handleCardClick, restartGame]
+  );
+
+  return memoizedValues;
+};
+
+export default useMemoryGameLogic;
