b7a52cccf3052d56226251fa6d0a65fb27c60bcf
ReenieSmooch <smolchenko@gmail.com>
2023-07-18 16:36:43 +0300

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { MemoryGameProvider } from "./context/MemoryGameContext";
-
 import Footer from "./components/Footer";
 import MemoryGame from "./components/Main";
 import Header from "./components/Header";
